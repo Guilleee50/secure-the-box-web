31545f381b0687855cbfda68f3a773265ac15a8a
Guillermo <04guillejm@gmail.com>
2026-04-29 18:21:42 +0200

    UPDATE Tabla Clasificacion

--- core/urls.py
+++ core/urls.py
@@ -18,12 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
-from api.views import register_view, docs_view, panel_view, validar_maquina_api, normativa_view, aceptar_normativa
+from api.views import register_view, docs_view, panel_view, validar_maquina_api, normativa_view, aceptar_normativa, home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="index.html"), name='home'),
-
+    # path('', TemplateView.as_view(template_name="index.html"), name='home'),
+    path('', home_view, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', register_view, name='register'),
